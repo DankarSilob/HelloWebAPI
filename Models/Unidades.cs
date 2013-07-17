f43bf665f07974ce42ec255c408bc73491302bbe
Daniel C. Sierra <dsierra.lob@gmail.com>
2013-07-17 07:59:07 -0700

    WEB API DEMO

---
+++ Models/Unidades.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWebAPI.Models
+{
+    public class Unidades
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public decimal latitud { get; set; }
+        public decimal longitud { get; set; }
+    }
+
+
+}
